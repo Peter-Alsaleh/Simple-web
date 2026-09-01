4d0090e465dffc71596193cd66a179e506ff940b
Peter-Alsaleh <meladbebo57@gmail.com>
2026-09-02 02:13:24 +0300

    Change server listen address to '0.0.0.0'

--- server.js
+++ server.js
@@ -93,6 +93,6 @@ app.delete("/api/tasks/:id", (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
